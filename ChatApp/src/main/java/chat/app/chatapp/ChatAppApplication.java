62b4e9ac48d6da1319e8b2cc2b2b0e1b98b5230a
aymen <aymen.rahali@esprit.tn>
2024-03-03 23:01:22 +0100

    add Listener connect/Disconnect

---
+++ ChatApp/src/main/java/chat/app/chatapp/ChatAppApplication.java
@@ -0,0 +1,14 @@
+package chat.app.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatAppApplication.class, args);
+    }
+
+
+}
---
+++ ChatApp/src/main/java/chat/app/chatapp/chat/ChatController.java
@@ -0,0 +1,9 @@
+package chat.app.chatapp.chat;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    
+}
